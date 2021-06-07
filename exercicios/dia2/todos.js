7821fc52a863bafade512bf8e6f0e48426bf0126
Eleandro Duzentos <eleandro@inbox.ru>
2021-06-07 10:37:20 +0100

    feat: added support for mysql and pagination on GET /todos

--- exercicios/dia2/todos.js
+++ exercicios/dia2/todos.js
@@ -1,18 +1,39 @@
 const todos = []
 
-module.exports = (app) => {
-  app.get('/todos', (_, res) => {
-    res.send({
-      code: 200,
-      meta: {
-        pagination: {
-          total: todos.length,
-          pages: 0,
-          page: 0,
-          limit: 0,
+module.exports = (app, connection) => {
+  app.get('/todos', (req, res) => {
+    const { limit, page } = req.query
+
+    const _limit = +limit
+    const _page = +page
+
+    connection.query('SELECT COUNT(id) FROM todos', (error, countResults, _) => {
+      if (error) {
+        throw error
+      }
+
+      const offset = (_page - 1) * _limit
+      const total = countResults[0]['COUNT(id)']
+      const pageCount = Math.ceil(total / limit)
+      
+      connection.query('SELECT * FROM todos LIMIT ?, ?', [offset, _limit], (error, results, _) => {
+        if (error) {
+          throw error
         }
-      },
-      data: todos
+
+        res.send({
+          code: 200,
+          meta: {
+            pagination: {
+              total: total,
+              pages: pageCount,
+              page: _page,
+              limit: _limit
+            }
+          },
+          data: results
+        })
+      })
     })
   })
 
