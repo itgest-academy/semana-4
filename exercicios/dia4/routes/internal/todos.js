d71f0f52f79274430140d296bb5c73d93c25eb02
Eleandro Duzentos <eleandro@inbox.ru>
2021-06-09 10:51:56 +0100

    feat: added GET /todos route

--- exercicios/dia4/routes/internal/todos.js
+++ exercicios/dia4/routes/internal/todos.js
@@ -1,7 +1,41 @@
 const router = require('express').Router()
 
+const db = require('../../db')
+
 router.get('/', (req, res) => {
-  
+  const { limit, page } = req.query
+
+  const _limit = +limit
+  const _page = +page
+
+  db.query('SELECT COUNT(id) FROM todos', (error, countResults, _) => {
+    if (error) {
+      throw error
+    }
+
+    const offset = (_page - 1) * _limit
+    const total = countResults[0]['COUNT(id)']
+    const pageCount = Math.ceil(total / limit)
+    
+    db.query('SELECT * FROM todos LIMIT ?, ?', [offset, _limit], (error, results, _) => {
+      if (error) {
+        throw error
+      }
+
+      res.send({
+        code: 200,
+        meta: {
+          pagination: {
+            total: total,
+            pages: pageCount,
+            page: _page,
+            limit: _limit
+          }
+        },
+        data: results
+      })
+    })
+  })
 })
 
 module.exports = router
