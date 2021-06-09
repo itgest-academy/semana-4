3bbb457538d0c746e2552036db5ede4fe9b6a4f5
Eleandro Duzentos <eleandro@inbox.ru>
2021-06-09 10:51:12 +0100

    feat: added bcrypt.hash for passwords

--- exercicios/dia4/routes/internal/users.js
+++ exercicios/dia4/routes/internal/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
-const db = require('../../db')
+const { validate } = require('indicative/validator')
+const { sanitize } = require('indicative/sanitizer')
+const bcrypt = require('bcrypt')
 
+const db = require('../../db')
 const auth = require('../../middlewares/auth')
 
 function removePasswordProperty(object) {
@@ -44,7 +47,19 @@ router.get('/', auth, (req, res) => {
   })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/search', (req, res) => {
+  const { name } = req.query
+
+  db.query(`SELECT * FROM users WHERE name LIKE "%${name}%";`, [name], (error, results) => {
+    if (error) {
+      throw error
+    }
+
+    res.send(results)
+  })
+})
+
+router.get('/:id', (req, res, next) => {
   const { id } = req.params
 
   db.query('SELECT * FROM users WHERE id = ? LIMIT 1', [id], (error, results, _) => {
@@ -55,28 +70,94 @@ router.get('/:id', (req, res) => {
     removePasswordProperty(results[0])
 
     res.send(results[0])
+  })
+})
+
+router.post('/', (req, res) => {
+  const user = req.body
+
+  validate(user, {
+    name: 'required',
+    email: 'required|email',
+    genre: 'required|boolean',
+    password: 'required|min:6',
+    passwordSame: 'required|same:password'
+  }).then((value) => {
+    sanitize(value, {
+      email: 'trim|lowerCase',
+      password: 'trim'
+    })
+
+    delete value.passwordSame
+
+    bcrypt.hash(value.password, 10).then((hash) => {
+      value.password = hash
+
+      db.query('INSERT INTO users SET ?', [value], (error, results, _) => {
+        if (error) {
+          throw error
+        }
+
+        const { insertId } = results
+
+        db.query('SELECT * FROM users WHERE id = ? LIMIT 1', [insertId], (error, results, _) => {
+          if (error) {
+            throw error
+          }
+
           removePasswordProperty(results[0])
 
           res.send(results[0])
+        })
+      })
+    }).catch((error) => { throw error })
+
+
+  }).catch((error) => {
+    res.status(400).send(error)
+  })
+})
 
 router.put('/:id', (req, res) => {
   const { id } = req.params
 
   const user = req.body
 
-  db.query('UPDATE users SET ? WHERE id = ?', [user, id], (error, results, _) => {
-    if (error) {
-      throw error
+  validate(user, {
+    email: 'email',
+    genre: 'boolean',
+    password: 'min:6',
+    passwordSame: 'requiredIf:password|same:password'
+  }).then(async (value) => {
+    sanitize(value, {
+      email: 'trim|lowerCase',
+      password: 'trim'
+    })
+
+    if (value.password) {
+      value.password = await bcrypt.hash(value.password, 10)
+
+      delete value.passwordSame
     }
 
-    db.query('SELECT * FROM users WHERE id = ? LIMIT 1', [id], (error, results, _) => {
+    db.query('UPDATE users SET ? WHERE id = ?', [value, id], (error, results, _) => {
       if (error) {
         throw error
       }
 
+      db.query('SELECT * FROM users WHERE id = ? LIMIT 1', [id], (error, results, _) => {
+        if (error) {
+          throw error
+        }
+
         removePasswordProperty(results[0])
 
         res.send(results[0])
+      })
+    })
+  }).catch((error) => {
+    res.status(400).send(error)
+  })
 })
 
 module.exports = router
